f55075a920ce54805fbafabd6d76b36133f59d30
Ruth <ruthkeane166@gmail.com>
2021-05-11 16:10:55 +0100

    initial commit

---
+++ AutomationApp/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿
+namespace AutomationApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_1 = new System.Windows.Forms.Button();
+            this.lbl_1 = new System.Windows.Forms.Label();
+            this.lbl_2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn_1
+            // 
+            this.btn_1.Location = new System.Drawing.Point(188, 100);
+            this.btn_1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btn_1.Name = "btn_1";
+            this.btn_1.Size = new System.Drawing.Size(169, 73);
+            this.btn_1.TabIndex = 0;
+            this.btn_1.Text = "Select Files";
+            this.btn_1.UseVisualStyleBackColor = true;
+            this.btn_1.Click += new System.EventHandler(this.btn_1_Click);
+            // 
+            // lbl_1
+            // 
+            this.lbl_1.AutoSize = true;
+            this.lbl_1.Location = new System.Drawing.Point(503, 117);
+            this.lbl_1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbl_1.Name = "lbl_1";
+            this.lbl_1.Size = new System.Drawing.Size(0, 17);
+            this.lbl_1.TabIndex = 1;
+            // 
+            // lbl_2
+            // 
+            this.lbl_2.AutoSize = true;
+            this.lbl_2.Location = new System.Drawing.Point(219, 194);
+            this.lbl_2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbl_2.Name = "lbl_2";
+            this.lbl_2.Size = new System.Drawing.Size(0, 17);
+            this.lbl_2.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.lbl_2);
+            this.Controls.Add(this.lbl_1);
+            this.Controls.Add(this.btn_1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_1;
+        private System.Windows.Forms.Label lbl_1;
+        private System.Windows.Forms.Label lbl_2;
+    }
+}
+
---
+++ AutomationApp/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace AutomationApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_1_Click(object sender, EventArgs e)
+        {
+            var fileContent = string.Empty;
+            //var filePath = string.Empty;
+            string[] filePath;
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = "c:\\";
+                openFileDialog.Filter = "excel|*.xls";
+                openFileDialog.Multiselect = true;
+                openFileDialog.FilterIndex = 2;
+                openFileDialog.RestoreDirectory = true;
+                  
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    //Get the path of specified file
+                    filePath = openFileDialog.FileNames;
+                    string filePathString = string.Join("\n", filePath);
+                    lbl_1.Text = filePathString;
+
+
+                    //for each in filePath
+                    //open file
+                    foreach (string sFileName in filePath)
+                    {
+                        //Create COM Objects. Create a COM object for everything that is referenced
+                        Excel.Application xlApp = new Excel.Application();
+                        xlApp.Visible = true;
+                        Excel.Workbook xlWorkbook = xlApp.Workbooks.Open(sFileName);
+                        Excel._Worksheet xlWorksheet = xlWorkbook.Sheets[1];
+                        Excel.Range xlRange = xlWorksheet.UsedRange;
+                        int rowCount = xlRange.Rows.Count;
+                        int colCount = xlRange.Columns.Count;
+                        
+                        xlRange.Sort(xlRange.Columns[3], Excel.XlSortOrder.xlAscending) ;
+                       // Excel.Range smallerRange = xlRange.AutoFilter(xlRange.Columns[3],"<100");
+
+                        
+                        //cleanup
+                        GC.Collect();
+                        GC.WaitForPendingFinalizers();
+                                               //rule of thumb for releasing com objects:
+                        //  never use two dots, all COM objects must be referenced and released individually
+                        //  ex: [somthing].[something].[something] is bad
+
+                        //release com objects to fully kill excel process from running in the background
+                       Marshal.ReleaseComObject(xlRange);
+                       Marshal.ReleaseComObject(xlWorksheet);
+
+                        //close and release
+                        //xlWorkbook.Close();
+                        Marshal.ReleaseComObject(xlWorkbook);
+
+                        //quit and release
+                        xlApp.Quit();
+                        Marshal.ReleaseComObject(xlApp);
+                  }
+                    // edit file
+                    // make word file
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
